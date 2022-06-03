0196593d8aed45d59cbb7dc3c0718b00605e2ba8
Nguyen Duc Quy <quynd@tech.est-rouge.com>
2022-06-03 13:24:03 +0700

    add empty

--- app/src/main/java/com/module/jetpack/compose/app/di/NavigationModule.kt
+++ app/src/main/java/com/module/jetpack/compose/app/di/NavigationModule.kt
@@ -1,13 +1,17 @@
 package com.module.jetpack.compose.app.di
 
+import com.module.jetpack.compose.detail.impl.di.DetailEntryModule
 import com.module.jetpack.compose.home.impl.di.HomeEntryModule
+import com.module.jetpack.compose.login.impl.di.LoginEntryModule
 import com.module.jetpack.compose.timetable.impl.di.TimetableEntryModule
 import dagger.Module
 
 @Module(
     includes = [
         HomeEntryModule::class,
-        TimetableEntryModule::class
+        TimetableEntryModule::class,
+        DetailEntryModule::class,
+        LoginEntryModule::class
     ]
 )
 interface NavigationModule
